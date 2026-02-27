f42be9d7f429d10a3e35f86e66352c4a52be29eb
reironex <nexreiro@gmail.com>
2026-02-28 06:21:09 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -10,83 +10,12 @@ const sleep = async (ms) => {
 
 const getState = () => {
   return [
-    {
-      key: 'datr',
-      value: randomString(24),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'ps_l',
-      value: Math.floor(Math.random() * 2).toString(),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString(),
-    },
-    {
-      key: 'ps_n',
-      value: Math.floor(Math.random() * 2).toString(),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'sb',
-      value: randomString(24),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'c_user',
-      value: randomString(14),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'fr',
-      value: `${randomString(8)}.${randomString(18)}.${randomString(8)}..AAA.0.0.${randomString(8)}.${randomString(12)}`,
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'xs',
-      value: `42%3A${randomString(10)}%3A2%3A${Math.floor(Date.now() / 1000)}%3A-1%3A7861`,
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
-    {
-      key: 'm_page_voice',
-      value: randomString(14),
-      domain: 'facebook.com',
-      path: '/',
-      hostOnly: true,
-      creation: new Date().toISOString(),
-      lastAccessed: new Date().toISOString()
-    },
+    { key: 'datr', value: randomString(24), domain: 'facebook.com', path: '/', hostOnly: true, creation: new Date().toISOString(), lastAccessed: new Date().toISOString() },
+    { key: 'ps_l', value: Math.floor(Math.random() * 2).toString(), domain: 'facebook.com', path: '/', hostOnly: true, creation: new Date().toISOString(), lastAccessed: new Date().toISOString() },
+    { key: 'sb', value: randomString(24), domain: 'facebook.com', path: '/', hostOnly: true, creation: new Date().toISOString(), lastAccessed: new Date().toISOString() },
+    { key: 'c_user', value: randomString(14), domain: 'facebook.com', path: '/', hostOnly: true, creation: new Date().toISOString(), lastAccessed: new Date().toISOString() },
+    { key: 'xs', value: `42%3A${randomString(10)}%3A2%3A${Math.floor(Date.now() / 1000)}%3A-1%3A7861`, domain: 'facebook.com', path: '/', hostOnly: true, creation: new Date().toISOString(), lastAccessed: new Date().toISOString() }
   ];
 };
 
-module.exports = {
-  randomString,
-  getState,
-  sleep
-};
+module.exports = { randomString, getState, sleep };
